76c2dd0ee20f7284afaa26466ebfa71587bd4908
PedroGeorgeCR <pedro465235@gmail.com>
2026-05-01 18:26:48 -0300

    Correção importação
    foi importado , after_this_request

--- baixarAnaliseProcessoGFL.py
+++ baixarAnaliseProcessoGFL.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from datetime import datetime
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, after_this_request
 from flask_cors import CORS
 from openpyxl import load_workbook
 
